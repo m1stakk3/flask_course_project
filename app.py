15b0ccfe9aac0d4a87baeb1a641e04658d807f23
Daniel Kirin <danielkirin@vk.com>
2023-05-31 17:12:30 +0300

    flask game

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, url_for
+from game_of_life import GameOfLife
+
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    height, width = 25, 25
+    if request.method == "POST":
+        height = int(request.form['height'])
+        width = int(request.form['width'])
+    global game
+    game = GameOfLife(height, width)
+    return render_template("index.html")
+
+
+@app.route("/live")
+def live():
+    game.form_new_generation()
+    game.counter += 1
+    return render_template("live.html", game=game)
+
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=5000, debug=True)
